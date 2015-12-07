901abcafd023386385ed1c582096c9bc38544a71
bouveram <maximebouverat@gmail.com>
2015-12-07 10:40:29 +0100

    Media Repository & tests

---
+++ FormationConsole/FormationNET/FormationNET/FormationLibrary/Media.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace FormationLibrary
+{
+    public abstract class Media : IEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+        public Publisher Publisher { get; set; }
+        public List<Author> Authors = new List<Author>();
+        public virtual double VATPrice
+        {
+            get {
+                return Price * 1.20;
+            }
+        }
+
+    }
+
+}
---
+++ FormationConsole/FormationNET/FormationNET/FormationUnitTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FormationLibrary;
+
+namespace FormationUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AuthorTest()
+        {
+            Author a = new Author { Id = 2, Name = "toto" };
+            Assert.AreEqual(2, a.Id);
+        }
+
+        [TestMethod]
+        public void BookTest()
+        {
+            Book b = new Book { Id = 2, NbPage = 5, Publisher = new Publisher { Id = 1, Name = "Super Publisher" }, Price = 10 };
+            Assert.AreEqual(5, b.NbPage);
+            Assert.AreEqual("Super Publisher", b.Publisher.Name);
+            Assert.AreEqual(10 * 1.05, b.VATPrice);
+        }
+
+        [TestMethod]
+        public void AddAuthorTest()
+        {
+            Book b = new Book { Id = 2, NbPage = 5, Publisher = new Publisher { Id = 1, Name = "Super Publisher" }, Price = 10 };
+            Author a = new Author { Id = 1, Name = "Jean Val Jean" };
+            b.Authors.Add(a);
+            Assert.AreEqual(1, b.Authors.Count);
+        }
+
+    }
+}
