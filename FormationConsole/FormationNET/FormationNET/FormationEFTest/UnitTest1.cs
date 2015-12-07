1c78865086a839b94b59eaf55962624b089d8121
bouveram <maximebouverat@gmail.com>
2015-12-07 16:19:46 +0100

    SQL Server connection + EntityFramework

---
+++ FormationConsole/FormationNET/FormationNET/FormationEFTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FormationEF;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FormationEFTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void EFTest()
+        {
+            FormationEntities entities = new FormationEntities();
+            List<Book> l = entities.Books.ToList();
+        }
+    }
+}
